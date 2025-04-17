e19149ecf273bb5452dc7fe717e08d7087f99419
陈文卓 <1142034529@qq.com>
2025-04-17 16:17:07 +0800

    低版本兼容

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // server.js
-const axios = require('axios');
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
